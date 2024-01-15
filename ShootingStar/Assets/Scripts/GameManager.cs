4d3d82cc2fa5dc93d499a747055266fb5c47194f
jungheol <jungheol@naver.com>
2024-01-15 15:10:04 +0900

    Enemy 랜덤 생성(시간 및 포인트) 구현

--- ShootingStar/Assets/Scripts/GameManager.cs
+++ ShootingStar/Assets/Scripts/GameManager.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour {
-	
-	
-	
+
+	public GameObject[] enemyObjects;
+	public Transform[] spawnPoints;
+
+	public float maxSpawnDelay;
+	public float curSpawnDelay;
+
+	private void Update() {
+		curSpawnDelay += Time.deltaTime;
+
+		if (curSpawnDelay > maxSpawnDelay) {
+			SpawnEnemy();
+			maxSpawnDelay = Random.Range(0.5f, 2.5f);
+			curSpawnDelay = 0;
+		}
+	}
+
+	private void SpawnEnemy() {
+		int ranEnemy = Random.Range(0, 3);
+		int ranPoint = Random.Range(0, 5);
+		Instantiate(enemyObjects[ranEnemy], spawnPoints[ranPoint].position, spawnPoints[ranPoint].rotation);
+	}
 }
